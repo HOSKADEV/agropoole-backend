5a382c291557c128e7292e4ed5eb1718d1aa6a92
Dashamon <dashamon@pop-os.localdomain>
2024-06-09 14:55:32 +0100

    first commit

---
+++ config/variables.php
@@ -0,0 +1,21 @@
+<?php
+  // Variables
+  return [
+    "creatorName" => "",
+    "creatorUrl" => "",
+    "templateName" => "Agropool",
+    "templateDescription" => "",
+    "templateKeyword" =>  "",
+    "licenseUrl" =>  "",
+    "livePreview" =>  "",
+    "productPage" =>  "",
+    "support" =>  "",
+    "moreThemes" =>  "",
+    "documentation" =>  "",
+    "repository" =>  "",
+    "facebookUrl" =>  "",
+    "twitterUrl" =>  "",
+    "githubUrl" =>  "",
+    "dribbbleUrl" =>  "",
+    "instagramUrl" =>  "",
+  ];
