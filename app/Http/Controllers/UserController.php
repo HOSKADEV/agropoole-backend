635f5318bcdd6ec01ce37722edaba6cd4fc3ba1d
AbdelaliKDM <khademallah.abdelali@gmail.com>
2024-08-03 18:04:17 +0100

    get only users with stocks

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,298 +2,321 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\PaginatedUserCollection;
-use App\Http\Resources\UserCollection;
-use App\Http\Resources\UserResource;
-use App\Models\User;
 use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\UserCollection;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\PaginatedUserCollection;
 
 class UserController extends Controller
 {
 
-  public function index(){
+  public function index()
+  {
     return view('content.users.list');
   }
-    //
-    public function update(Request $request){
+  //
+  public function update(Request $request)
+  {
 
-      $request->mergeIfMissing(['user_id' => auth()->user()->id]);
+    $request->mergeIfMissing(['user_id' => auth()->user()->id]);
 
-      $validator = Validator::make($request->all(), [
-        //'firstname' => 'sometimes|string',
-        //'lastname' => 'sometimes|string',
-        'user_id' => 'required|exists:users,id',
-        'city_id' => 'sometimes|exists:cities,id',
-        'name' => 'sometimes|string',
-        'phone' => 'sometimes|numeric',
-        /* 'phone' => ['sometimes','numeric','digits:10',Rule::unique('users')->ignore($user->id)], */
-        //'email' => ['sometimes','email',Rule::unique('users')->ignore($user->id)],
-        'image' => 'sometimes|mimetypes:image/*',
-        'status' => 'sometimes|in:1,2,3',
+    $validator = Validator::make($request->all(), [
+      //'firstname' => 'sometimes|string',
+      //'lastname' => 'sometimes|string',
+      'user_id' => 'required|exists:users,id',
+      'city_id' => 'sometimes|exists:cities,id',
+      'name' => 'sometimes|string',
+      'phone' => 'sometimes|numeric',
+      /* 'phone' => ['sometimes','numeric','digits:10',Rule::unique('users')->ignore($user->id)], */
+      //'email' => ['sometimes','email',Rule::unique('users')->ignore($user->id)],
+      'image' => 'sometimes|mimetypes:image/*',
+      'status' => 'sometimes|in:1,2,3',
 
-        'enterprise_name' => 'sometimes',
-        'longitude' => 'sometimes',
-        'latitude' => 'sometimes',
-      ]);
+      'enterprise_name' => 'sometimes',
+      'longitude' => 'sometimes',
+      'latitude' => 'sometimes',
+    ]);
 
-      if ($validator->fails()){
-        return response()->json([
-            'status' => 0,
-            'message' => $validator->errors()->first()
-          ]
-        );
-      }
-
-      try{
-
-        $user = User::find($request->user_id);
-
-        if($request->status == 3){
-          $request->merge(['fcm_token' => null]);
-          $user->tokens()->delete();
-        }
-
-        $user->update($request->except('image'));
-
-        if($request->hasFile('image')){
-            $path = $request->image->store('/uploads/users/images','upload');
-
-            /* $file = $request->image;
-            $name = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension();
-            $filename = 'users/' . $user->id . '/' . md5(time().$name) . '.' . $extension;
-            $url = $this->firestore($file->get(),$filename);
-            $user->image = $url; */
-
-            $user->image = $path;
-
-            $user->save();
-        }
-
-        return response()->json([
-          'status' => 1,
-          'message' => 'success',
-          'data' => new UserResource($user)
-        ]);
-
-      }catch(Exception $e){
-        return response()->json([
+    if ($validator->fails()) {
+      return response()->json(
+        [
           'status' => 0,
-          'message' => $e->getMessage()
-        ]
-      );
-      }
-
-    }
-
-    public function delete(Request $request){
-
-      $validator = Validator::make($request->all(), [
-        'user_id' => 'required',
-      ]);
-
-      if ($validator->fails()){
-        return response()->json([
-            'status' => 0,
-            'message' => $validator->errors()->first()
-          ]
-        );
-      }
-
-      try{
-
-        $user = User::findOrFail($request->user_id);
-
-        $user->delete();
-
-        return response()->json([
-          'status' => 1,
-          'message' => 'success',
-        ]);
-
-      }catch(Exception $e){
-        return response()->json([
-          'status' => 0,
-          'message' => $e->getMessage()
-        ]
-      );
-      }
-
-    }
-
-    public function restore(Request $request){
-
-      $validator = Validator::make($request->all(), [
-        'user_id' => 'required',
-      ]);
-
-      if ($validator->fails()){
-        return response()->json([
-            'status' => 0,
-            'message' => $validator->errors()->first()
-          ]
-        );
-      }
-
-      try{
-
-        $user = User::withTrashed()->findOrFail($request->user_id);
-
-        $user->restore();
-
-        return response()->json([
-          'status' => 1,
-          'message' => 'success',
-          'data' => new UserResource($user)
-        ]);
-
-      }catch(Exception $e){
-        return response()->json([
-          'status' => 0,
-          'message' => $e->getMessage()
-        ]
-      );
-      }
-
-    }
-
-    public function change_password(Request $request){
-
-      $validator = Validator::make($request->all(), [
-        'old_password' => 'required',
-        'new_password' => 'required|min:8|confirmed',
-      ]);
-
-
-
-      if ($validator->fails()) {
-        return response()->json([
-          'status'=> 0,
           'message' => $validator->errors()->first()
-        ]);
-
-      }
-
-        $user = auth()->user();
-
-        if(Hash::check($request->old_password, $user->password)){
-
-          $user->password = Hash::make($request->new_password);
-          $user->save();
-
-          return response()->json([
-            'status'=> 1,
-            'message' => 'password changed'
-          ]);
-
-        }else{
-
-          return response()->json([
-            'status'=> 0,
-            'message' => 'wrong password'
-          ]);
-
-        }
-
+        ]
+      );
     }
 
-    public function reset_password(Request $request){
+    try {
 
-      $validator = Validator::make($request->all(), [
-        'user_id' => 'required|exists:users,id',
-      ]);
+      $user = User::find($request->user_id);
 
-
-
-      if ($validator->fails()) {
-        return response()->json([
-          'status'=> 0,
-          'message' => $validator->errors()->first()
-        ]);
-
-      }
-
-      try{
-        $user = User::findOrFail($request->user_id);
-        $user->password = Hash::make($user->name?? $user->email);
-        $user->save();
-
-        return response()->json([
-          'status'=> 1,
-          'message' => 'password reseted'
-        ]);
-      }catch(Exception $e){
-
-        return response()->json([
-          'status'=> 0,
-          'message' => $e->getMessage()
-        ]);
-
-      }
-
-    }
-
-    public function deactivate(Request $request){
-      try{
-
-        $user = $request->user();
-
-        $user->update(['status' => 2 , 'email' => null , 'fcm_token' => null]);
-
+      if ($request->status == 3) {
+        $request->merge(['fcm_token' => null]);
         $user->tokens()->delete();
-
-        return response()->json([
-          'status'=> 1,
-          'message' => 'success',
-        ]);
-      }catch(Exception $e){
-        return response()->json([
-          'status'=> 0,
-          'message' => $e->getMessage(),
-        ]);
       }
 
-    }
+      $user->update($request->except('image'));
 
-    public function get(Request $request){
-      $validator = Validator::make($request->all(), [
-        'role' => 'required|in:1,2,3,4,5',
+      if ($request->hasFile('image')) {
+        $path = $request->image->store('/uploads/users/images', 'upload');
+
+        /* $file = $request->image;
+        $name = $file->getClientOriginalName();
+        $extension = $file->getClientOriginalExtension();
+        $filename = 'users/' . $user->id . '/' . md5(time().$name) . '.' . $extension;
+        $url = $this->firestore($file->get(),$filename);
+        $user->image = $url; */
+
+        $user->image = $path;
+
+        $user->save();
+      }
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'success',
+        'data' => new UserResource($user)
       ]);
 
-      if ($validator->fails()){
-        return response()->json([
-            'status' => 0,
-            'message' => $validator->errors()->first()
-          ]
-        );
-      }
-
-      try{
-
-        $users = User::where('role', $request->role);
-
-
-
-        $users = $request->has('all') ? new UserCollection($users->get())
-                                      : new PaginatedUserCollection($users->paginate(10));
-
-        return response()->json([
-          'status' => 1,
-          'message' => 'success',
-          'data' => $users
-        ]);
-
-      }catch(Exception $e){
-        return response()->json([
+    } catch (Exception $e) {
+      return response()->json(
+        [
           'status' => 0,
           'message' => $e->getMessage()
         ]
       );
-      }
     }
+
+  }
+
+  public function delete(Request $request)
+  {
+
+    $validator = Validator::make($request->all(), [
+      'user_id' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $validator->errors()->first()
+        ]
+      );
+    }
+
+    try {
+
+      $user = User::findOrFail($request->user_id);
+
+      $user->delete();
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'success',
+      ]);
+
+    } catch (Exception $e) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $e->getMessage()
+        ]
+      );
+    }
+
+  }
+
+  public function restore(Request $request)
+  {
+
+    $validator = Validator::make($request->all(), [
+      'user_id' => 'required',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $validator->errors()->first()
+        ]
+      );
+    }
+
+    try {
+
+      $user = User::withTrashed()->findOrFail($request->user_id);
+
+      $user->restore();
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'success',
+        'data' => new UserResource($user)
+      ]);
+
+    } catch (Exception $e) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $e->getMessage()
+        ]
+      );
+    }
+
+  }
+
+  public function change_password(Request $request)
+  {
+
+    $validator = Validator::make($request->all(), [
+      'old_password' => 'required',
+      'new_password' => 'required|min:8|confirmed',
+    ]);
+
+
+
+    if ($validator->fails()) {
+      return response()->json([
+        'status' => 0,
+        'message' => $validator->errors()->first()
+      ]);
+
+    }
+
+    $user = auth()->user();
+
+    if (Hash::check($request->old_password, $user->password)) {
+
+      $user->password = Hash::make($request->new_password);
+      $user->save();
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'password changed'
+      ]);
+
+    } else {
+
+      return response()->json([
+        'status' => 0,
+        'message' => 'wrong password'
+      ]);
+
+    }
+
+  }
+
+  public function reset_password(Request $request)
+  {
+
+    $validator = Validator::make($request->all(), [
+      'user_id' => 'required|exists:users,id',
+    ]);
+
+
+
+    if ($validator->fails()) {
+      return response()->json([
+        'status' => 0,
+        'message' => $validator->errors()->first()
+      ]);
+
+    }
+
+    try {
+      $user = User::findOrFail($request->user_id);
+      $user->password = Hash::make($user->name ?? $user->email);
+      $user->save();
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'password reseted'
+      ]);
+    } catch (Exception $e) {
+
+      return response()->json([
+        'status' => 0,
+        'message' => $e->getMessage()
+      ]);
+
+    }
+
+  }
+
+  public function deactivate(Request $request)
+  {
+    try {
+
+      $user = $request->user();
+
+      $user->update(['status' => 2, 'email' => null, 'fcm_token' => null]);
+
+      $user->tokens()->delete();
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'success',
+      ]);
+    } catch (Exception $e) {
+      return response()->json([
+        'status' => 0,
+        'message' => $e->getMessage(),
+      ]);
+    }
+
+  }
+
+  public function get(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'role' => 'required|in:1,2,3,4,5',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $validator->errors()->first()
+        ]
+      );
+    }
+
+    try {
+
+      $users = User::where('role', $request->role);
+
+
+      if ($request->role != '5') {
+        $users = $users->join('stocks', 'users.id', 'stocks.user_id')
+          ->select('users.*', DB::raw('COUNT(stocks.user_id)'))
+          ->groupBy('stocks.user_id');
+      }
+
+
+      $users = $request->has('all') ? new UserCollection($users->get())
+        : new PaginatedUserCollection($users->paginate(10));
+
+      return response()->json([
+        'status' => 1,
+        'message' => 'success',
+        'data' => $users
+      ]);
+
+    } catch (Exception $e) {
+      return response()->json(
+        [
+          'status' => 0,
+          'message' => $e->getMessage()
+        ]
+      );
+    }
+  }
 
 }
